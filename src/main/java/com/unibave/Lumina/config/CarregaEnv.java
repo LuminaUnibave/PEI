34499c6624002118c77cbe8cff35e8268f8cab47
LuccaPMuller <128193086+LuccaPMuller@users.noreply.github.com>
2025-05-07 09:43:24 -0300

    Adicionar .env e gitignore

---
+++ src/main/java/com/unibave/Lumina/config/CarregaEnv.java
@@ -0,0 +1,16 @@
+package com.unibave.Lumina.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class CarregaEnv {
+
+    @PostConstruct
+    public void carregaEnv(){
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    }
+
+
+}
