9beaea29002e5b46a6a7031a34982e8fb3550f48
Lucca Paladini Muller <luccapmuller@gmail.com>
2025-11-09 11:32:57 -0300

    Ajuste

--- src/main/backend/com/unibave/Lumina/DTOs/Usuario/UsuarioAtualizarDTO.java
+++ src/main/backend/com/unibave/Lumina/DTOs/Usuario/UsuarioAtualizarDTO.java
@@ -1,5 +1,6 @@
 package com.unibave.Lumina.DTOs.Usuario;
 
+import com.unibave.Lumina.enums.TpUsuario;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -14,12 +15,23 @@ import lombok.NoArgsConstructor;
 @Schema(description = "DTO para atualizar usuário (UsuarioAtualizarDTO)")
 public class UsuarioAtualizarDTO {
 
+    @Schema(description = "Id do usuario", example = "2")
+    @NotNull
+    private Long id;
+
+    @Schema(description = "Nome do usuário", example = "Gabriel")
+    @NotBlank
+    private String nome;
+
     @Schema(description = "Email do usuário", example = "gabrielsilva@gmail.com")
     @Email(message = "Email inválido")
     private String email;
 
     @Schema(description = "Senha do usuário", example = "$enh@")
     @NotBlank
-    @NotNull
     private String senha;
+
+    @Schema(description = "Tipo do usuário", example = "OPERADOR")
+    @NotNull
+    private TpUsuario tpUsuario;
 }
--- src/main/backend/com/unibave/Lumina/config/SecurityConfig.java
+++ src/main/backend/com/unibave/Lumina/config/SecurityConfig.java
@@ -1,16 +1,12 @@
 package com.unibave.Lumina.config;
 
-import com.unibave.Lumina.model.component.JwtFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -21,26 +17,14 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Autowired
-    private JwtFilter jwtFilter;
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.configure(http))
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/api/auth/login").permitAll()
-                        .requestMatchers("/api/auth/validar-credenciais").permitAll()
-                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll() // Swagger
-                        .anyRequest().authenticated()
-                )
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+                        .anyRequest().permitAll()
+                );
 
         return http.build();
     }
-
 }
