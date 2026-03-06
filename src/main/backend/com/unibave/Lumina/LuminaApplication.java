367b8e70f45b30e6e9ccb5bf441b1f973843ebd4
Antonio Marcos Goulart <antoniomarcos5674335@gmail.com>
2026-03-06 19:29:57 -0300

    Passado informacoes sensiveis para o .env

--- src/main/backend/com/unibave/Lumina/LuminaApplication.java
+++ src/main/backend/com/unibave/Lumina/LuminaApplication.java
@@ -14,6 +14,13 @@ public class LuminaApplication {
             System.setProperty("DB_URL", dotenv.get("DB_URL"));
             System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
             System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+            //SMTP
+            System.setProperty("SMTP_MAIL", dotenv.get("SMTP_MAIL"));
+            System.setProperty("SMTP_PORT", dotenv.get("SMTP_PORT"));
+            System.setProperty("SMTP_USERNAME", dotenv.get("SMTP_USERNAME"));
+            System.setProperty("SMTP_PASSWORD", dotenv.get("SMTP_PASSWORD"));
+            System.setProperty("SMTP_PROTOCOL", dotenv.get("SMTP_PROTOCOL"));
+
 
         SpringApplication.run(LuminaApplication.class, args);
 
