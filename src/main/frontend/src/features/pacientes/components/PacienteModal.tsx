9b2f333cc843f3419221d6fc16d4ce50c86bc822
Alehandro <alehandro.leandro@gmail.com>
2026-05-09 15:34:58 -0300

    commit para eviar conflitos

--- src/main/frontend/src/features/pacientes/components/PacienteModal.tsx
+++ src/main/frontend/src/features/pacientes/components/PacienteModal.tsx
@@ -1,2 +1,2 @@
-import { PacientePayload, PacienteRespostaDTO, Situacao } from '../../../core/types';
+import { PacientePayload, PacienteRespostaDTO, Situacao } from '../../../core/types'; //será adicionado posteriormente por outro dev
 import { Modal } from '../../../components/ui/Modal';
