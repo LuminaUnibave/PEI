2bb5e2aef39973ebd7aafb78b1272ae5ffd1a1ab
Alehandro <alehandro.leandro@gmail.com>
2026-06-07 20:18:27 -0300

    feat: habilitando CORS

--- src/main/java/com/unibave/Lumina/config/SecurityConfig.java
+++ src/main/java/com/unibave/Lumina/config/SecurityConfig.java
@@ -20,6 +20,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authz -> authz
                         .anyRequest().permitAll()
