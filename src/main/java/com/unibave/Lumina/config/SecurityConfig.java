17c0b086c5a94a986abec97108595dea8a722155
Lucca Paladini Muller <128193086+LuccaPMuller@users.noreply.github.com>
2026-06-25 09:40:23 -0300

    Remove CORS configuration from SecurityConfig
    Removed CORS configuration bean from SecurityConfig.

--- src/main/java/com/unibave/Lumina/config/SecurityConfig.java
+++ src/main/java/com/unibave/Lumina/config/SecurityConfig.java
@@ -35,18 +35,4 @@ public class SecurityConfig {
                 )
                 .build();
     }
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-    
-        configuration.setAllowedOriginPatterns(List.of("*"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
-    
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-    
-        return source;
-    }
 }
